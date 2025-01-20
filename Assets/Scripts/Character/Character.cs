5b7273ea4160637d29ff6f2e327c30a120385480
James Kerrane <hi@jameskerrane.com>
2025-01-20 18:20:26 -0500

    Refactor Character.cs
    - Rename _yarnNode to yarnNode
    - Move into "Character" namespace
    - Remove unnused Update(), Start()

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -2,31 +2,21 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Yarn.Unity;
 
-public class Character : MonoBehaviour,
-    IPointerClickHandler
+namespace Character
 {
-    [SerializeField]
-    private string _yarnNode;
-    public void OnPointerClick(PointerEventData eventData)
+    public class Character : MonoBehaviour,
+        IPointerClickHandler
     {
-        DialogueHelper.Instance.DialogueRunner.StartDialogue(_yarnNode);
-    }
+        [SerializeField] private string yarnNode;
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            DialogueHelper.Instance.DialogueRunner.StartDialogue(yarnNode);
+        }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    [YarnCommand("set_node")]
-    public void SetNode(string node)
-    {
-        _yarnNode = node;
+        [YarnCommand("set_node")]
+        public void SetNode(string node)
+        {
+            yarnNode = node;
+        }
     }
 }
