08b36e884360cc0744c99c5ce1988260e5350551
James Kerrane <hi@jameskerrane.com>
2025-01-20 19:03:21 -0500

    Refactor ClueObjectUI.cs
    - Remove unused min/max sizes
    - Fix namespace not corresponding to file location
    - Add TODO tags to unfinished functions
    - Use explicit modifiers

--- Assets/Scripts/Clues/ClueObjectUI.cs
+++ Assets/Scripts/Clues/ClueObjectUI.cs
@@ -1,82 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ClueObjectUI : MonoBehaviour, 
-    IDragHandler, IBeginDragHandler, IEndDragHandler,
-    IScrollHandler, IPointerDownHandler, IPointerUpHandler
+namespace Clues
 {
-    [SerializeField]
-    private static readonly float _sizeMin = .25f;
-    [SerializeField]
-    private static readonly float _sizeMax = 1.5f;
-
-    private Vector2 _offset;
-    private bool _mouseDown;
-
-    
-
-    // Start is called before the first frame update
-    void Start()
+    public class ClueObjectUI : MonoBehaviour, 
+        IDragHandler, IBeginDragHandler, IEndDragHandler,
+        IScrollHandler, IPointerDownHandler, IPointerUpHandler
     {
-        transform.parent = ClueBoardManager.Instance.HoldingPinTransform;
-        transform.localPosition = Vector3.zero;
-        ClueBoardManager.Instance.AddToBin(this);
-        _mouseDown = false;
-    }
+        private Vector2 _offset;
+        private bool _mouseDown;
 
-    void StartDelay()
-    {
-        ClueBoardManager.Instance.AddToBin(this);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = eventData.position + _offset;
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Vector2 mousePos = eventData.pressPosition;
-        Vector2 uiPos = transform.position;
-        _offset = uiPos - mousePos;
-
-        transform.parent = ClueBoardManager.Instance.BoardTransform;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        _offset = Vector2.zero;
-    }
-
-    public void OnScroll(PointerEventData eventData)
-    {
-        // TODO
-        // Get resizing working for clueobjects
-        if (_mouseDown)
+        // Start is called before the first frame update
+        private void Start()
         {
-            Debug.Log("Yippee!");
+            transform.parent = ClueBoardManager.Instance.HoldingPinTransform;
+            transform.localPosition = Vector3.zero;
+            ClueBoardManager.Instance.AddToBin(this);
+            _mouseDown = false;
         }
-        else
+
+        // TODO: Never used. remove?
+        private void StartDelay()
         {
-            ClueBoardManager.Instance.OnScroll(eventData);
+            ClueBoardManager.Instance.AddToBin(this);
         }
-    }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        _mouseDown = true;
-    }
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.position + _offset;
+        }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        _mouseDown = false;
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Vector2 mousePos = eventData.pressPosition;
+            Vector2 uiPos = transform.position;
+            _offset = uiPos - mousePos;
+
+            transform.parent = ClueBoardManager.Instance.BoardTransform;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _offset = Vector2.zero;
+        }
+
+        public void OnScroll(PointerEventData eventData)
+        {
+            // TODO
+            // Get resizing working for ClueObjects
+            if (_mouseDown)
+            {
+                Debug.Log("Yippee!");
+            }
+            else
+            {
+                ClueBoardManager.Instance.OnScroll(eventData);
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _mouseDown = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _mouseDown = false;
+        }
     }
 }
