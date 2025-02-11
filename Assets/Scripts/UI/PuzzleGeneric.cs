a3a333d58441476ed6627599ac160aa0cb8b6409
Esther Moon <113371188+rmoon45@users.noreply.github.com>
2025-02-10 20:23:11 -0500

    Added blank puzzle screen prefab and script

--- Assets/Scripts/UI/PuzzleGeneric.cs
+++ Assets/Scripts/UI/PuzzleGeneric.cs
@@ -4,6 +4,11 @@ public class PuzzleGeneric : MonoBehaviour
 {
     [SerializeField] private GameObject puzzleScreenObj;
     
+    public void ActivatePuzzleScreen() {
+        // actually wait is it instantiate or set active hmmm a real thinker 
+        puzzleScreenObj.SetActive(true);
+    }
+
     public void DisablePuzzleScreen() {
         puzzleScreenObj.SetActive(false);
     }
